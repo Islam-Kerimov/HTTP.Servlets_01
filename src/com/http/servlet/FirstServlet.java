ffa239f2e4885c6bddef958d67d79298175317b9
kerimovikh <kerimovikh@mail.ru>
2022-04-15 15:20:49 +0300

    POST request with body

--- src/com/http/servlet/FirstServlet.java
+++ src/com/http/servlet/FirstServlet.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -38,8 +40,14 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        System.out.println(parameterMap);
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+//        System.out.println(parameterMap);
+
+        try (BufferedReader reader = req.getReader();
+             Stream<String> lines = reader.lines()) {
+
+            lines.forEach(System.out::println);
+        }
     }
 
     @Override
